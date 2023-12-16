634a1ec0e0b84eb72084202afcfa25e4ed0b6bdc
shinyingrid <shinyingrid88@gmail.com>
2023-12-16 12:17:42 +0530

    Jenkins practice commit

---
+++ src/test/java/Jenkins_Package/Jenkins_Sample1.java
@@ -0,0 +1,14 @@
+package Jenkins_Package;
+
+import org.testng.annotations.Test;
+
+public class Jenkins_Sample1 {
+
+	@Test
+	public void jenkinsMethod1()
+	{
+		System.out.println("Jenkins Sample Program");
+
+	}
+
+}
